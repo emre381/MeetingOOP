66bba75b31c95054c92ef05c70a16fe7e3b4083a
emre381 <cihatemre381@gmail.com>
2024-09-26 19:43:16 +0300

    first commit

---
+++ Program1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+     class Program
+    {
+        class Insan { 
+        private string isim;
+        private int yas;
+        public void selamver()
+        {
+            Console.WriteLine("Merhaba");
+        }
+        public void bilgileriSor()
+        {
+            Console.WriteLine("Adın ne ve kaç yaşındasın ? ");
+        }
+        public void cevapVer()
+        {
+            Console.WriteLine("Benim adım"+ isim + "ve yaşım" + yas );
+        }
+        public void isimVeYasDuzenleme(string isim, int yas)
+        {
+            this.isim = isim;
+            this.yas = yas; 
+        }
+        }
+        static void Main(string[] args)
+        {
+            Insan i1 = new Insan();
+            Insan i2 = new Insan();
+            i1.isimVeYasDuzenleme("Emre",24);
+            i2.isimVeYasDuzenleme("Ahmet", 33);
+
+            i1.selamver();
+            i2.selamver();
+            i1.bilgileriSor();
+            i2.cevapVer();
+            i2.bilgileriSor();
+            i1.cevapVer();
+
+            Console.ReadLine();
+
+        }
+    }
+}
